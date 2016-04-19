1ac7f4172e6069845b7203690af7aec18dc8241c
yinhonglu <yinghong1991@126.com>
2016-04-19 16:00:50 +0800

    增加获取两个日期之间的间隔天数的接口

--- kakaoBase/src/main/java/com/kakao/utils/DateUtils.java
+++ kakaoBase/src/main/java/com/kakao/utils/DateUtils.java
@@ -211,6 +211,25 @@ public final class DateUtils {
 			return "刚刚";
 		}
 	}
+	
+        public static int getGapDay(Date startDate, Date endDate) {
+        	Calendar fromCalendar = Calendar.getInstance();
+        	fromCalendar.setTime(startDate);
+        	fromCalendar.set(Calendar.HOUR_OF_DAY, 0);
+        	fromCalendar.set(Calendar.MINUTE, 0);
+        	fromCalendar.set(Calendar.SECOND, 0);
+        	fromCalendar.set(Calendar.MILLISECOND, 0);
+
+        	Calendar toCalendar = Calendar.getInstance();
+        	toCalendar.setTime(endDate);
+        	toCalendar.set(Calendar.HOUR_OF_DAY, 0);
+        	toCalendar.set(Calendar.MINUTE, 0);
+        	toCalendar.set(Calendar.SECOND, 0);
+        	toCalendar.set(Calendar.MILLISECOND, 0);
+        	
+        	return (int) ((toCalendar.getTime().getTime() - fromCalendar.getTime()
+                	.getTime()) / (1000 * 60 * 60 * 24));
+        }
 
 	public static void usageAndTest() {
 		String  TAG = "DateUtils";
